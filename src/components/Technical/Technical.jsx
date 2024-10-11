6699c7ed54a35a87b5c5ec23e86d4471bae0ca3c
hashtagp <pritamd2708@gmail.com>
2024-10-11 11:53:17 +0530

    team

--- src/components/Technical/Technical.jsx
+++ src/components/Technical/Technical.jsx
@@ -7,44 +7,55 @@ const Technical = () => {
       <div className="line1">
         <div className="member">
           <h1>Member</h1>
-          <img src={assets.profile_icon} alt="" />
+          <img src={assets.chanakya} alt="" />
           <p>Kasa Chanakya</p>
           <ul>
-            <li><a href=""><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="https://www.linkedin.com/in/kasa-chanakya
+"><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="https://github.com/chanakya29092004"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         <div className="member">
           <h1>Chair</h1>
-          <img src={assets.profile_icon} alt="" />
+          <img src={assets.pritam} alt="" />
           <p>Pritam Dhar</p>
           <ul>
-            <li><a href=""><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="www.linkedin.com/in/pritam-dhar-300b5725b"><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href=""><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         <div className="member">
           <h1>Member</h1>
-          <img src={assets.profile_icon} alt="" />
+          <img src={assets.rachan} alt="" />
           <p>Rachan Murthy</p>
           <ul>
-            <li><a href=""><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="www.linkedin.com/in/rachan-murthy-726788256
+"><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="https://github.com/RachanMurthy
+"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         </div>
         <div className="line2">
         <div className="member">
           <h1>Member</h1>
-          <img src={assets.profile_icon} alt="" />
+          <img src={assets.satwik} alt="" />
           <p>Satwik M Badigar</p>
           <ul>
-            <li><a href=""><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="https://www.linkedin.com/in/satwik-m-badiger
+"><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="https://github.com/satwikmbadiger
+"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         <div className="member">
           <h1>Member</h1>
-          <img src={assets.profile_icon} alt="" />
+          <img src={assets.satyam} alt="" />
           <p>Satyam Gupta</p>
           <ul>
-            <li><a href=""><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="https://www.linkedin.com/in/satyam-gupta-41606a28a
+"><img src={assets.linkedIn} alt="" /></a></li>
+            <li><a href="https://github.com/Satyamgupta2365"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         </div>
--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -23,10 +23,13 @@ const Team = () => {
       <div className="sub-team">
         <div className="member">
           <h1>President</h1>
-          <img src={assets.profile_icon} alt="President's profile" />
+          <img src={assets.anjum} alt="President's profile" />
           <p>T S Anjum Kausar</p>
           <ul>
-            <li><a href="#"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://www.linkedin.com/in/ts-anjum-kausar-303b49270/
+"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://github.com/tsanjumkausar
+"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
       </div>
@@ -34,18 +37,23 @@ const Team = () => {
       <div className="sub-team">
         <div className="member">
           <h1>Vice President</h1>
-          <img src={assets.profile_icon} alt="Vice President's profile" />
+          <img src={assets.rahul} alt="Vice President's profile" />
           <p>M Rahul Sagayaraj</p>
           <ul>
-            <li><a href="#"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="www.linkedin.com/in/m-rahul-sagayaraj-870756281
+"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://github.com/rahulsags
+"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         <div className="member">
           <h1>Vice President</h1>
-          <img src={assets.profile_icon} alt="Vice President's profile" />
+          <img src={assets.tarun} alt="Vice President's profile" />
           <p>Tarun Kumar S</p>
           <ul>
-            <li><a href="#"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://www.linkedin.com/in/tarun-kumar-s-676a74267/"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://quanta-naut.github.io/
+"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
       </div>
@@ -53,26 +61,33 @@ const Team = () => {
       <div className="sub-team">
         <div className="member">
           <h1>Secretary</h1>
-          <img src={assets.profile_icon} alt="Secretary's profile" />
+          <img src={assets.venkateshwara} alt="Secretary's profile" />
           <p>Venkateshwar Reddy</p>
           <ul>
-            <li><a href="#"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://www.linkedin.com/in/venkateshwar-reddy-b7290717b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app
+"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://github.com/namo12345"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         <div className="member">
           <h1>Joint Secretary</h1>
-          <img src={assets.profile_icon} alt="Joint Secretary's profile" />
+          <img src={assets.deekshitha} alt="Joint Secretary's profile" />
           <p>Deekshitha Gowda</p>
           <ul>
-            <li><a href="#"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://www.linkedin.com/in/deekshitha-ravikumar-gowda-679080295/
+"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="https://github.com/16-dee
+"><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
         <div className="member">
           <h1>Treasurer</h1>
-          <img src={assets.profile_icon} alt="Treasurer's profile" />
+          <img src={assets.umaiza} alt="Treasurer's profile" />
           <p>Umaiza Fathima</p>
           <ul>
-            <li><a href="#"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href="http://linkedin.com/in/umaiza-fathima-4247b9329
+"><img src={assets.linkedIn} alt="LinkedIn" /></a></li>
+            <li><a href=""><img src={assets.github} alt="Github" /></a></li>
           </ul>
         </div>
       </div>
