9f2654bc3d53098415c37553c0bd9321483798a0
Rachan Murthy <rachan3thi@gmail.com>
2024-10-13 21:24:26 +0530

    Update Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,29 +5,54 @@ import { Link } from 'react-router-dom'
 
 const Navbar = ({menu,setMenu}) => {
 
+  const handleMenuClick = (menuName) => {
+    setMenu(menuName);
+    // Additional logic to close the menu can be added here
+  };
+
   return (
-    <div className='navbar'>
+    <nav className='navbar'>
       <div className="logo">
-        <img src={assets.logo} alt="" />
+        <img src={assets.logo} alt="HACC Logo" />
         <h1>HACC</h1>
       </div>
       <input type="checkbox" id="check" />
       <div className="btn_one">
-        <label htmlFor="check" >
-        <img src={assets.braces} alt="" />
+        <label htmlFor="check" aria-label="Toggle navigation">
+          <img src={assets.braces} alt="Menu Toggle" />
         </label>
       </div>
       <div className="navs">
         <ul>
-            <li><label htmlFor="check" >&#123;</label></li>
-            <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}><Link to="/">Home</Link></li>
-            <li onClick={()=>setMenu("Hackathon")} className={menu==="Hackathon"?"active":""}><Link to="/hackathon">Hackathon</Link></li>
-            <li onClick={()=>setMenu("Coding")} className={menu==="Coding"?"active":""}><Link to="/coding">Coding</Link></li>
-            <li onClick={()=>setMenu("Team")} className={menu==="Team"?"active":""}><Link to="/team">Team</Link></li>
-            <li><label htmlFor="check" >&#125;</label></li>
+          <li><label htmlFor="check">&#123;</label></li>
+          <li 
+            onClick={() => handleMenuClick("Home")} 
+            className={menu === "Home" ? "active" : ""}
+          >
+            <Link to="/">Home</Link>
+          </li>
+          <li 
+            onClick={() => handleMenuClick("Hackathon")} 
+            className={menu === "Hackathon" ? "active" : ""}
+          >
+            <Link to="/hackathon">Hackathon</Link>
+          </li>
+          <li 
+            onClick={() => handleMenuClick("Coding")} 
+            className={menu === "Coding" ? "active" : ""}
+          >
+            <Link to="/coding">Coding</Link>
+          </li>
+          <li 
+            onClick={() => handleMenuClick("Team")} 
+            className={menu === "Team" ? "active" : ""}
+          >
+            <Link to="/team">Team</Link>
+          </li>
+          <li><label htmlFor="check">&#125;</label></li>
         </ul>
       </div>
-    </div>
+    </nav>
   )
 }
 
