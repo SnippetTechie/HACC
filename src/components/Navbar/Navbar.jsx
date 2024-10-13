9f81f844cd7eed4e76e868fd68b3c23dc9067867
hashtagp <pritamd2708@gmail.com>
2024-10-13 22:46:39 +0530

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import './Navbar.css'
-import assets from '../../assets/assets'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Navbar.css';
+import assets from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({menu,setMenu}) => {
+const Navbar = ({ menu, setMenu }) => {
+  const [isChecked, setIsChecked] = useState(false); 
+
+  const handleMenuClick = (menuOption) => {
+    setMenu(menuOption); 
+    setIsChecked(false); 
+  };
 
   return (
     <div className='navbar'>
@@ -11,24 +17,53 @@ const Navbar = ({menu,setMenu}) => {
         <img src={assets.logo} alt="" />
         <h1>HACC</h1>
       </div>
-      <input type="checkbox" id="check" />
+      <input 
+        type="checkbox" 
+        id="check" 
+        checked={isChecked} 
+        onChange={() => setIsChecked(!isChecked)} 
+      />
       <div className="btn_one">
-        <label htmlFor="check" >
-        <img src={assets.braces} alt="" />
+        <label htmlFor="check">
+          <img src={assets.braces} alt="" />
         </label>
       </div>
       <div className="navs">
         <ul>
-            <li><label htmlFor="check" >&#123;</label></li>
-            <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}><Link to="/">Home</Link></li>
-            <li onClick={()=>setMenu("Hackathon")} className={menu==="Hackathon"?"active":""}><Link to="/hackathon">Hackathon</Link></li>
-            <li onClick={()=>setMenu("Coding")} className={menu==="Coding"?"active":""}><Link to="/coding">Coding</Link></li>
-            <li onClick={()=>setMenu("Team")} className={menu==="Team"?"active":""}><Link to="/team">Team</Link></li>
-            <li><label htmlFor="check" >&#125;</label></li>
+          <li>
+            <label htmlFor="check"><p>{"{"}</p></label>
+          </li>
+          <li 
+            onClick={() => handleMenuClick("Home")} 
+            className={menu === "Home" ? "active" : ""}
+          >
+            <Link to="/"><label htmlFor="check">Home</label></Link>
+          </li>
+          <li 
+            onClick={() => handleMenuClick("Hackathon")} 
+            className={menu === "Hackathon" ? "active" : ""}
+          >
+            <Link to="/hackathon"><label htmlFor="check">Hackathon</label></Link>
+          </li>
+          <li 
+            onClick={() => handleMenuClick("Coding")} 
+            className={menu === "Coding" ? "active" : ""}
+          >
+            <Link to="/coding"><label htmlFor="check">Coding</label></Link>
+          </li>
+          <li 
+            onClick={() => handleMenuClick("Team")} 
+            className={menu === "Team" ? "active" : ""}
+          >
+            <Link to="/team"><label htmlFor="check">Team</label></Link>
+          </li>
+          <li>
+            <label htmlFor="check"><p>{"}"}</p></label>
+          </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
