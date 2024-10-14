cd3a96a92acd7f38b1c44d386b7e094a78e6c76a
Rachan Murthy <rachan3thi@gmail.com>
2024-10-14 20:21:19 +0530

    Update TeamPage

--- src/components/MemberCard/MemberCard.jsx
+++ src/components/MemberCard/MemberCard.jsx
@@ -4,25 +4,31 @@ import assets from '../../assets/assets';
 
 const MemberCard = ({ role, name, image, linkedin, github }) => (
   <div className="member">
-    <h1>{role}</h1>
-    <img src={image} alt={`${name}'s profile`} />
-    <p>{name}</p>
-    <ul>
-      {linkedin && (
-        <li>
-          <a href={linkedin} target="_blank" rel="noopener noreferrer">
-            <img src={assets.linkedIn} alt="LinkedIn" />
-          </a>
-        </li>
-      )}
-      {github && (
-        <li>
-          <a href={github} target="_blank" rel="noopener noreferrer">
-            <img src={assets.github} alt="Github" />
-          </a>
-        </li>
-      )}
-    </ul>
+    <div className="member-title">
+      <h1>{role}</h1>
+    </div>
+    <div className="member-info">
+      <img src={image} alt={`${name}'s profile`} />
+      <p>{name}</p>
+    </div>
+    <div className="member-links">
+      <ul>
+        {linkedin && (
+          <li>
+            <a href={linkedin} target="_blank" rel="noopener noreferrer">
+              <img src={assets.linkedIn} alt="LinkedIn" />
+            </a>
+          </li>
+        )}
+        {github && (
+          <li>
+            <a href={github} target="_blank" rel="noopener noreferrer">
+              <img src={assets.github} alt="Github" />
+            </a>
+          </li>
+        )}
+      </ul>
+    </div>
   </div>
 );
 
