0a6ad2c887c032775b4a41927ea756b2d60257eb
Pritam <138203240+hashtagp@users.noreply.github.com>
2024-10-16 09:27:01 +0530

    Hackathons added

--- src/pages/Hackathon/Hackathon.jsx
+++ src/pages/Hackathon/Hackathon.jsx
@@ -19,9 +19,8 @@ const Hackathon = () => {
         <div className="hackathons">
             <h1>Upcoming Hackathons</h1>
             <ul>
-                <li><button className='date'>15 Oct</button>HACC Hackathon<a href='https://forms.office.com/r/r5Ar4VtLra'><button className="submit">Apply</button></a></li>
-                <li><button className='date'>15 Oct</button>Shridevi Hackathon<a href='https://hackathon.shritek.com/'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>18 Oct</button>MINDSPARK'24<a href='https://www.mind-spark.org/events/robotica/'><button className="submit">Apply</button></a></li>
+                <li><button className='date'>20 Oct</button>QUANT-A-MAZE<a href='https://www.quantamaze.dev/'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>20 Oct</button>Hack Overflow 2.0<a href='https://rns-hackoverflow-2.devfolio.co/'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>21 Oct</button>Mercuria Hackathon<a href='https://hackathon.mercuria.com/india-2024/'><button className="submit">Apply</button></a></li>
             </ul>
