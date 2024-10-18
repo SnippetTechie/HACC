1846f4aeb2213523e520e51a96128e3eb760047b
hashtagp <pritamd2708@gmail.com>
2024-10-19 00:02:59 +0530

    hackathons update

--- src/pages/Hackathon/Hackathon.jsx
+++ src/pages/Hackathon/Hackathon.jsx
@@ -19,7 +19,6 @@ const Hackathon = () => {
         <div className="hackathons">
             <h1>Upcoming Hackathons</h1>
             <ul>
-                <li><button className='date'>18 Oct</button>MINDSPARK'24<a href='https://www.mind-spark.org/events/robotica/'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>20 Oct</button>QUANT-A-MAZE<a href='https://www.quantamaze.dev/'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>20 Oct</button>Hack Overflow 2.0<a href='https://rns-hackoverflow-2.devfolio.co/'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>21 Oct</button>Mercuria Hackathon<a href='https://hackathon.mercuria.com/india-2024/'><button className="submit">Apply</button></a></li>
