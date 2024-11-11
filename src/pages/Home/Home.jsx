d89dfea48fdc370fcc242a0fd7375c5e3333a404
Rachan Murthy <rachan3thi@gmail.com>
2024-11-11 22:17:39 +0530

    integrate sliderimage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import HeroSection from '../../components/HeroSection.jsx';
 import WelcomeSection from '../../components/WelcomeSection.jsx';
 import CardsSection from '../../components/CardsSection.jsx';
+import ImageSlider from '../../components/ImageSlider/ImageSlider.jsx';
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <ImageSlider />
       <WelcomeSection />
       <CardsSection />
     </div>
