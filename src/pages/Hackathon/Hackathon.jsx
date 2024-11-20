d5af20da001d4c4600f6edbd773fc59654449ca0
hashtagp <hashtag.p27@gmail.com>
2024-11-20 08:53:56 +0530

    hackathon update

--- src/pages/Hackathon/Hackathon.jsx
+++ src/pages/Hackathon/Hackathon.jsx
@@ -19,8 +19,7 @@ const Hackathon = () => {
         <div className="hackathons">
             <h1>Upcoming Hackathons</h1>
             <ul>
-                <li><button className='date'>8 Nov</button>ARGONYX '24<a href='https://www.argonyx.in/'><button className="submit">Apply</button></a></li>
-                <li><button className='date'>14 Nov</button>Amazon Smbhav<a href='https://amazonsmbhav2024.hackerearth.com/'><button className="submit">Apply</button></a></li>
+                <li><button className='date'>24 Nov</button>Py Jam<a href='https://forms.gle/WKGa23TSnuPxQHUX7'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>25 Nov</button>Unfold 2024<a href='https://bit.ly/Unfold24'><button className="submit">Apply</button></a></li>
                 <li><button className='date'>20 Feb</button>Hack Overflow 2.0<a href='https://rns-hackoverflow-2.devfolio.co/'><button className="submit">Apply</button></a></li>
             </ul>
