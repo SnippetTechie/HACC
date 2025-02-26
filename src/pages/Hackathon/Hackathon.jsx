cc58c9a7b271575d41be7f9f690eecd9961c96f0
hashtagp <hashtag.p27@gmail.com>
2025-02-26 19:50:27 +0530

    update upcoming hackathons with new events and dates

--- src/pages/Hackathon/Hackathon.jsx
+++ src/pages/Hackathon/Hackathon.jsx
@@ -19,11 +19,8 @@ const Hackathon = () => {
         <div className="hackathons">
             <h1>Upcoming Hackathons</h1>
             <ul>
-                <li><button className='date'>25 Jan</button>HACK-A-LEAGUE 3.0<a href='https://unstop.com/p/hack-a-league-30-global-academy-of-technology-1346539'><button className="submit">Apply</button></a></li>
-                <li><button className='date'>26 Jan</button>Thiran 2025<a href='https://thiran.sece.ac.in'><button className="submit">Apply</button></a></li>
-                <li><button className='date'>05 Feb</button>AceHack 4.0<a href='https://acehack.uem.edu.in'><button className="submit">Apply</button></a></li>
-                <li><button className='date'>10 Feb</button>Codefest 2025<a href='https://codefest.aitm.edu.in'><button className="submit">Apply</button></a></li>
-                <li><button className='date'>20 Feb</button>Hack Overflow 2.0<a href='https://rns-hackoverflow-2.devfolio.co/'><button className="submit">Apply</button></a></li>
+                <li><button className='date'>13 Mar</button>HaccVerse'25<a href='https://haccverse.hacc.co.in'><button className="submit">Apply</button></a></li>
+                <li><button className='date'>29 Mar</button>AceHack 4.0<a href='https://acehack.uem.edu.in'><button className="submit">Apply</button></a></li>
             </ul>
         </div>
     </div>
